7547fcbadf864c64a0bebc740185b2df1ead7e4b
Christian Iversen <ci@iversenit.dk>
2023-05-25 21:00:26 +0200

    Allow run.sh to pass extra arguments to ankerctl.py, and exec into it, to avoid a /bin/sh instance.

--- ankerctl/data/run.sh
+++ ankerctl/data/run.sh
@@ -4,4 +4,4 @@ CONFIG_PATH=/data/options.json
 
 export FLASK_HOST="$(jq --raw-output '.flask_host // empty' $CONFIG_PATH)"
 
-/app/ankerctl.py webserver run
+exec /app/ankerctl.py webserver run "${@}"
